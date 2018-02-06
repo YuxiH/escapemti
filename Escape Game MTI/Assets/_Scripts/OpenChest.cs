d4ea9a3588be74b6802bc1a32c1415ee270f5aa0
Yuxi <maxime.huang.yuxi@gmail.com>
2018-02-06 11:58:34 +0100

    keyscript: fix bad displayu

--- Escape Game MTI/Assets/_Scripts/OpenChest.cs
+++ Escape Game MTI/Assets/_Scripts/OpenChest.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class OpenChest : MonoBehaviour {
-    
+
+    public string solution;
     public GameObject player;
     public GameObject inp;
     public InputField input;
+    public GameObject top;
+    public Text score;
+    private bool open;
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("player"))
+        if (other.CompareTag("player") && !open)
         {
             UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController sc = player.GetComponent<UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController>();
             sc.mouseLook.SetCursorLock(false);
@@ -27,5 +31,25 @@ public class OpenChest : MonoBehaviour {
         inp.SetActive(false);
         input.text = "";
         input.placeholder.GetComponent<Text>().text = "";
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (input.text == solution && !open)
+        {
+            open = true;
+            player.GetComponent<PlayerScript>().keys++;
+            score.text = "Keys : " + player.GetComponent<PlayerScript>().keys;
+            inp.SetActive(false);
+        }
+    }
+    
+    void Update()
+    {
+        if (open)
+        {
+            if (top.transform.eulerAngles.z >= 300 || top.transform.eulerAngles.z == 0)
+                top.transform.eulerAngles -= new Vector3(0, 0, 0.5f);
+        }
     }
 }
