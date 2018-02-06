a041fd1b83bf9b0a6bf295e2b48a4235cf5ed9e2
Yuxi <maxime.huang.yuxi@gmail.com>
2018-02-06 22:57:04 +0100

    sound: Add sound riddle event

---
+++ Escape Game MTI/Assets/_Scripts/SoundScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundScript : MonoBehaviour {
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("player"))
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("player"))
+        {
+            GetComponent<AudioSource>().Stop();
+        }
+    }
+}
