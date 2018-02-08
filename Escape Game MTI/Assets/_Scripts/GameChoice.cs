9167f229e90230ec7b69af0506301dfe94a65ad5
Yuxi <maxime.huang.yuxi@gmail.com>
2018-02-08 21:26:48 +0100

    Menu: Add contextual menu

---
+++ Escape Game MTI/Assets/_Scripts/GameChoice.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameChoice : MonoBehaviour {
+
+    public bool brave = false;
+    public GameObject yesButton;
+    public GameObject noButton;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    void Start()
+    {
+        yesButton.GetComponent<Button>().onClick.AddListener(TaskOnClick);
+        noButton.GetComponent<Button>().onClick.AddListener(TaskOnClick2);
+    }
+
+    void TaskOnClick()
+    {
+        brave = true;
+        SceneManager.LoadScene("MainGame", LoadSceneMode.Single);
+    }
+
+    void TaskOnClick2()
+    {
+        brave = false;
+        SceneManager.LoadScene("MainGame", LoadSceneMode.Single);
+    }
+}
