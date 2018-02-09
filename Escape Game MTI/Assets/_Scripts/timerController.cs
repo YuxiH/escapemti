f12ba9980b5304999886a44a1148ed33a2a786e2
Valentin LAMBERT <valentin1.lambert@epita.fr>
2018-02-09 16:18:37 +0100

    Timer: Add a timer

---
+++ Escape Game MTI/Assets/_Scripts/timerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timerController : MonoBehaviour {
+	public float time =10f;
+	bool CountDownOn = true;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (timer ());
+	}
+
+	IEnumerator timer()
+	{
+		while (time > 0) {
+			time--;
+			yield return new WaitForSeconds (1f);
+			GetComponent<Text> ().text = string.Format("{0:0}:{1:00}", Mathf.Floor(time/60), time % 60);
+		}
+	}
+}
